2109be46a6594e37e1b5ca2dbee2206c24597a94
Iamneo <techadmin@iamneo.ai>
2023-09-12 08:05:54 +0000

    Backup Commit

---
+++ my-spring-project/src/main/java/com/example/MyBean.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class MyBean {
+    private String message;
+
+    // Getter and Setter methods
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
